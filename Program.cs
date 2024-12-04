a0ec17eb938bff20c20aa9bc6c3c08d95e920f34
Shreyansh Koladiya <55000207+shreyu1701@users.noreply.github.com>
2024-12-04 15:48:47 -0500

    Added: Program.cs file

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using Home_Health_Device_Data_Logger.Views;
+
+namespace Home_Health_Device_Data_Logger
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new LoginPage());
+        }
+    }
+}
